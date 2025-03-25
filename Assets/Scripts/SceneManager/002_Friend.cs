a060d5f78f328fb82e71fcfb4d99d948ec652563
GitSub <vrc.maya@chungbuk.ac.kr>
2025-03-26 00:10:07 +0900

    |asdf

--- Assets/Scripts/SceneManager/002_Friend.cs
+++ Assets/Scripts/SceneManager/002_Friend.cs
@@ -8,12 +8,12 @@ public class FriendListManager : MonoBehaviour
 
     void Start()
     {
-        for (int i = 0; i < 10; i++)
+        for (int i = 0; i < 20; i++)
         {
             GameObject card = Instantiate(friendCardPrefab, contentParent);
             card.transform.localScale = Vector3.one;
 
-            card.transform.Find("NameText").GetComponent<TextMeshProUGUI>().text = "가나다";
+            card.transform.Find("NameText").GetComponent<TextMeshProUGUI>().text = "asdf";
             card.transform.Find("CountText").GetComponent<TextMeshProUGUI>().text = $"No.{i + 1}";
         }
     }
